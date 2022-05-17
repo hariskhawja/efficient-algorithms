444ca317c9c7ec7129b7b377ce185691dfa32962
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2022-05-17 17:56:02 -0400

    opened ascendingSort program
    reconsidering the type of sort

---
+++ ascendingSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <list>
+
+void ascendingSort(int* array) {
+
+    for(int round = 0; round < sizeof(&array) / sizeof(&array[0]); round++) {
+
+        for(int element = 0; round < sizeof(&array) / sizeof(&array[0]); element++) {
+            break;
+        }
+
+    }
+
+}
+
+int main() {
+
+    int array[] = {2, 3, 4, 5, 6, 7};
+    ascendingSort(array);
+
+    return 0;
+}
--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <bits/stdc++.h>
 
-/*
 int binarySearch(int numbers[], int target) {
     for(int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++) {
         if(numbers[i] == target) {
@@ -10,13 +8,12 @@ int binarySearch(int numbers[], int target) {
     }
     return 0;
 }
-*/
 
 int main() {
 
     int nums[] = {5, 2, 14, 5, 6, 2, 6, 7};
     int target = 5;
-    std::sort(nums, nums + sizeof(nums) / sizeof(nums[0]));
+    //std::sort(nums, nums + sizeof(nums) / sizeof(nums[0]));
 
     //std::cout << binarySearch(nums, target);
 
