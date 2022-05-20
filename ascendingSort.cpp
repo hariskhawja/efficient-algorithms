a133b1ad2c02e5f0db11a627c2b6f32ca9263f95
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2022-05-20 13:04:18 -0400

    playing around with pointers

--- ascendingSort.cpp
+++ ascendingSort.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <list>
 
-void ascendingSort(int* array) {
+void ascendingSort(int* test) {
 
+    /*
     for(int round = 0; round < sizeof(&array) / sizeof(&array[0]); round++) {
 
         for(int element = 0; round < sizeof(&array) / sizeof(&array[0]); element++) {
@@ -10,13 +11,22 @@ void ascendingSort(int* array) {
         }
 
     }
+    */
 
+
+   *test += 1;
+
+   std::cout << *test;
+   std::cout << test;
 }
 
 int main() {
 
     int array[] = {2, 3, 4, 5, 6, 7};
-    ascendingSort(array);
+
+    int test = 51;
+
+    ascendingSort(&test);
 
     return 0;
 }
