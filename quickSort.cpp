2d169b0bec8cb2eefdebeef046eeaa14a0bd0407
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2022-05-25 08:47:24 -0400

    testing

--- quickSort.cpp
+++ quickSort.cpp
@@ -22,29 +22,31 @@ int main() {
 
     std::getline(std::cin, text);
 
+    std::string num = "";
+
     for(int part = 0; part < text.size(); part++) {
 
-        std::string num = "";
-
         if(isspace(text[part])) {
-            //std::cout << std::stoi(num) << std::endl;
-            std::cout << text[part] << std::endl;
+            //std::cout << text[part] << std::endl;
             int x = std::stoi(num);
-            std::cout << x << std::endl;
+            //std::cout << x << s\td::endl;
             vect.push_back(x);
+            num = "";
         }
 
         else {
-            std::cout << text[part] << std::endl;
+            //std::cout << text[part] << std::endl;
             num += text[part];
-            std::cout << "NUM: " << num << std::endl;
+            //std::cout << "NUM: " << num << std::endl;
         }
 
     }
 
+    std::cout << vect.size() << std::endl;
+
     for(int i = 0; i < vect.size(); i++) {
 
-        std::cout << vect[i];
+        std::cout << vect[i] << std::endl;
 
     }
 
